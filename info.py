97f21a74d81b0a58da425e170f73fec69b91357f
PRIME BOT'z <sahidhasannayem@gmail.com>
2024-11-06 21:28:19 +0600

    Update info.py

--- info.py
+++ info.py
@@ -13,8 +13,8 @@ def is_enabled(value, default):
 #---------------------------------------------------------------
 #---------------------------------------------------------------         ,
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '25425840'))
-API_HASH = environ.get('API_HASH', 'e6ea2eca4aa38e965511f323e5ffa578')
+API_ID = int(environ.get('API_ID', ''))
+API_HASH = environ.get('API_HASH', '')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
