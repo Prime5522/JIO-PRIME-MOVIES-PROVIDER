6f611ddd3c8d17d753cd74544b30d39528ac9376
PRIME BOT'z <sahidhasannayem@gmail.com>
2024-11-26 19:43:14 +0600

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -95,7 +95,7 @@ async def send_movie_updates(bot, file_name, caption, file_id):
         
         btn = [
             [InlineKeyboardButton('🎥 𝗚𝗲𝘁 𝗙𝗶𝗹𝗲 📂', url=f'https://t.me/{temp.U_NAME}?start=pm_mode_file_{ADMINS[0]}_{file_id}')],
-            [InlineKeyboardButton('🔰 𝗠𝗼𝘃𝗶𝗲 𝗦𝗲𝗮𝗿𝗰𝗵 𝗚𝗿𝗼𝘂𝗽 🔰', url='https://t.me/+OG3sftDEbZ9kMzFl')]
+            [InlineKeyboardButton('🔰 𝗠𝗼𝘃𝗶𝗲 𝗦𝗲𝗮𝗿𝗰𝗵 𝗚𝗿𝗼𝘂𝗽 🔰', url='https://t.me/Prime_Movies4U/143')]
         ]
         reply_markup = InlineKeyboardMarkup(btn)
         
